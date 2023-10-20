904609b765896e19d71cb068f2c3913043d8efe3
isel06002 <isel06002@E-PW0234HX>
2023-10-20 12:03:22 +0200

    Add project files.

---
+++ Inlämning4.10/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Inlämning_4.__10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv hur långa kateterna ska vara");
+            int längd = int.Parse(Console.ReadLine());
+            for(int i = 0;i<längd;i++)
+            {
+                for(int j = 0; j <i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("*");
+            }
+        }
+    }
+}
